37c3bc12d65bdf5b973e1c5377a08c60b1fb3000
Alex Lourie <djay.il@gmail.com>
2014-06-11 22:10:41 +0930

    Updated with oh-my-zsh role
    Signed-off-by: Alex Lourie <djay.il@gmail.com>

--- devbox_setup.sh
+++ devbox_setup.sh
@@ -5,8 +5,13 @@ sudo apt-get upgrade -y
 
 sudo apt-get install -y ansible cmake
 
-for role in Ansibles.vim Ansibles.build-essential Ansibles.git Ansibles.python; do sudo ansible-galaxy install $role; done
+for role in Ansibles.vim Ansibles.build-essential Ansibles.git Ansibles.python devbox.oh-my-zsh; do sudo ansible-galaxy install $role; done
+
+mkdir ~/Projects
+cd ~/Projects
+
+git clone git://github.com/alourie/devbox_ansible
+cd devbox_ansible
 
 echo 'localhost   ansible_connection=local' > inventory
-
-ansible-playbook -K -i inventory playbook.yml
+ansible-playbook -K -i inventory mybook.yml
